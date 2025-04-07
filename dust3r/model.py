7a79353b14077bf4fc4b62a96e9b9f8323396f4a
Elvis Ohemeng Gyaase <48416104+StepLogic@users.noreply.github.com>
2025-04-07 12:53:33 -0400

    update

--- dust3r/model.py
+++ dust3r/model.py
@@ -208,3 +208,17 @@ class AsymmetricCroCo3DStereo (
 
         res2['pts3d_in_other_view'] = res2.pop('pts3d')  # predict view2's pts3d in view1's frame
         return res1, res2
+
+    def forward_pass_with_features(self, view1, view2):
+        # encode the two images --> B,S,D
+        (shape1, shape2), (feat1, feat2), (pos1, pos2) = self._encode_symmetrized(view1, view2)
+
+        # combine all ref images into object-centric representation
+        dec1, dec2 = self._decoder(feat1, pos1, feat2, pos2)
+
+        with torch.cuda.amp.autocast(enabled=False):
+            res1 = self._downstream_head(1, [tok.float() for tok in dec1], shape1)
+            res2 = self._downstream_head(2, [tok.float() for tok in dec2], shape2)
+
+        res2['pts3d_in_other_view'] = res2.pop('pts3d')  # predict view2's pts3d in view1's frame
+        return res1, res2,feat1, feat2
